10b2ff000f04dc25ae650c3c3006df2be894f2c4
wchenqi <15395381923@163.com>
2026-08-24 14:44:43 +0800

    脚本自动识别单端双端数据，输出时建立子文件夹

--- bulkRNA/04_bulkRNA_Clean_fastp_parallel.sh
+++ bulkRNA/04_bulkRNA_Clean_fastp_parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #### 脚本说明
-# conda activate fastp_env
+## conda activate /data/med-hancs/apps/anaconda3/2022.10/envs/fastp_env
 ## https://github.com/OpenGene/fastp
 ## https://www.jianshu.com/p/bfb573fcb3ec
 
@@ -12,8 +12,9 @@ module load java/10.0.2
 #echo ${SP[@]}
 
 ## 基本参数
-OUTDIR="/scratch/2026-08-03/med-wangcq/SelfUse/Heart/01_GEO/GSE95143/01Result/01RNAseq/02Fastp/"
-INDIR="/scratch/2026-08-03/med-wangcq/SelfUse/Heart/01_GEO/GSE95143/00Data/RNAseq/02Fastq/"
+OUTDIR="/scratch/2026-08-19/med-wangcq/Others/Hancs/GSE193516/03Fastp/"
+INDIR="/scratch/2026-08-19/med-wangcq/Others/Hancs/GSE193516/00RawData/02_fastq/"
+suffix=".fastq"
 THREAD=30
 JOBS=$((THREAD / 8))
 ForWhippet="True"
@@ -34,57 +35,101 @@ fi
 export OUTDIR INDIR N_num
 
 # 获取样本列表
-find $INDIR -mindepth 1 -maxdepth 1 -type f -name "*_1.fastq.gz" | \
-xargs -I {} -P ${JOBS} bash -c '
-    # 这里是压缩文件_1.fastq.gz
-    fq1="$1";
+find $INDIR -mindepth 1 -type f -name "*${suffix}" | \
+xargs -P ${JOBS} -n 1 bash -c '
+    # 这里是压缩文件${suffix}, 包含单端双端测序结果
+    fq1="$1"
     echo "处理文件: ${fq1}"
     
-    # 提取样本名（去掉目录和后缀），例如 SRR123
-    sp=$(basename "$fq1" "_1.fastq.gz")
-    # sp=${fq1%_1.fastq.gz}
-    # echo ${sp}
-
-    # fq2路径
-    fq2=${fq1/_1.fastq.gz/_2.fastq.gz}
-    echo "配对样本: ${fq2}"
-
-    # 快速检查 R2 是否存在，不存在则跳过
-    if [ ! -f "${fq2}" ]; then
-        echo "警告: 找不到 ${fq2}, 跳过 ${sp}"
-        continue
-    fi
-
     # 外部变量传参
     outdir='${OUTDIR}';
     indir='${INDIR}';
     N_num='${N_num}';
 
-    # 运行 fastp
-    fastp \
-        -w 8 \
-        -i ${fq1} \
-        -I ${fq2} \
-        -o ${outdir}/${sp}_1.fastq.gz \
-        -O ${outdir}/${sp}_2.fastq.gz \
-        --trim_front1 6 \
-        --trim_tail1 0 \
-        --trim_front2 6 \
-        --trim_tail2 0 \
-        --dedup \
-        --overrepresentation_analysis \
-        --qualified_quality_phred 28 \
-        --unqualified_percent_limit 40 \
-        --length_required 30 \
-        --complexity_threshold 30 \
-        --cut_window_size 4 \
-        --cut_mean_quality 30 \
-        --cut_front \
-        --n_base_limit $N_num \
-        --detect_adapter_for_pe \
-        --trim_poly_g \
-        --poly_g_min_len 10 \
-        --trim_poly_x \
-        --poly_x_min_len 10 \
-        --html "${outdir}/${sp}_fastp_report.html"
-' _ {}
+    # 提取文件名
+    filename=$(basename "$fq1")
+    
+    # 如果文件后缀包含"_1", 作为双端数据处理
+    if [[ ${filename} == *_1${suffix} ]]; then 
+        
+        # 构建fq2路径
+        fq2=${fq1/_1${suffix}/_2${suffix}}
+        echo "配对样本: ${fq2}"
+
+        # 提取样本名（去掉目录和后缀），例如 SRR123
+        sp=$(basename "$fq1" "_1${suffix}")
+
+        # 快速检查 R2 是否存在，不存在则跳过
+        if [ ! -f "${fq2}" ]; then
+            echo "警告: 找不到 ${fq2}, 跳过 ${sp}"
+            continue
+        else
+            # 如果fq2文件存在, 建立子文件夹 'pairedEnd'
+            outdir_PE="${outdir}/pairedEnd/"
+            echo "输出路径 ${outdir_PE}"
+            if [ ! -d "${outdir_PE}" ]; then
+                mkdir -p ${outdir_PE}
+            fi
+        fi
+
+        # 运行 fastp
+        fastp \
+            -w 8 \
+            -i ${fq1} \
+            -I ${fq2} \
+            -o ${outdir}/${sp}_1${suffix} \
+            -O ${outdir}/${sp}_2${suffix} \
+            --trim_front1 6 \
+            --trim_tail1 0 \
+            --trim_front2 6 \
+            --trim_tail2 0 \
+            --overrepresentation_analysis \
+            --qualified_quality_phred 28 \
+            --unqualified_percent_limit 40 \
+            --length_required 30 \
+            --complexity_threshold 30 \
+            --cut_window_size 4 \
+            --cut_mean_quality 30 \
+            --cut_front \
+            --n_base_limit $N_num \
+            --detect_adapter_for_pe \
+            --trim_poly_g \
+            --poly_g_min_len 10 \
+            --trim_poly_x \
+            --poly_x_min_len 10 \
+            --html "${outdir_PE}/${sp}_fastp_report.html"
+    else
+        # 如果fq2文件存在, 建立子文件夹 'pairedEnd'
+        outdir_SE="${outdir}/singleEnd/"
+        echo "输出路径 ${outdir_SE}"
+        if [ ! -d "${outdir_SE}" ]; then
+            mkdir -p ${outdir_SE}
+        fi
+
+        # 提取样本名（去掉目录和后缀），例如 SRR123
+        sp=$(basename "$fq1" "${suffix}")
+
+        # 运行 fastp
+        fastp \
+            -w 8 \
+            -i ${fq1} \
+            -o ${outdir}/${sp}${suffix} \
+            --trim_front1 6 \
+            --trim_tail1 0 \
+            --overrepresentation_analysis \
+            --qualified_quality_phred 28 \
+            --unqualified_percent_limit 40 \
+            --length_required 30 \
+            --complexity_threshold 30 \
+            --cut_window_size 4 \
+            --cut_mean_quality 30 \
+            --cut_front \
+            --n_base_limit $N_num \
+            --detect_adapter_for_pe \
+            --trim_poly_g \
+            --poly_g_min_len 10 \
+            --trim_poly_x \
+            --poly_x_min_len 10 \
+            --html "${outdir_SE}/${sp}_fastp_report.html"
+    fi
+' _
